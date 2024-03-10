3f3cf8b64d7a7ed2ec24372b156a6349f7affa4a
Agrey Tosira <agreytosira@gmail.com>
2024-03-10 10:52:53 +0700

    style: applying typography size

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 function App() {
   return (
     <div className='container'>
-      <h1 className='text-2xl font-bold text-blue-600'>Test</h1>
+      <h1>Heading 1</h1>
+      <h2>Heading 2 </h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <h5>Heading 5</h5>
+      <h6>Heading 6</h6>
+      <p>paragraph</p>
     </div>
   )
 }
