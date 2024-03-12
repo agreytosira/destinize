fbe672483d55df8e1911f35be68c2357f5b2b196
Agrey Tosira <agreytosira@gmail.com>
2024-03-12 14:57:32 +0700

    fix: add alt text on sponsor logo image

--- src/sections/Sponsor.jsx
+++ src/sections/Sponsor.jsx
@@ -1,11 +1,11 @@
 function Sponsor() {
   return (
     <div className='container flex items-center justify-between'>
-      <img src='./sponsor1.png' className='object-contain' alt='' />
-      <img src='./sponsor2.png' className='object-contain' alt='' />
-      <img src='./sponsor3.png' className='object-contain' alt='' />
-      <img src='./sponsor4.png' className='object-contain' alt='' />
-      <img src='./sponsor5.png' className='object-contain' alt='' />
+      <img src='./sponsor1.png' className='object-contain' alt='Logo Sponsor 1' />
+      <img src='./sponsor2.png' className='object-contain' alt='Logo Sponsor 2' />
+      <img src='./sponsor3.png' className='object-contain' alt='Logo Sponsor 3' />
+      <img src='./sponsor4.png' className='object-contain' alt='Logo Sponsor 4' />
+      <img src='./sponsor5.png' className='object-contain' alt='Logo Sponsor 5' />
     </div>
   )
 }
