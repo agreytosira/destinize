883fd17e3ed87d6f5528f59df1dd3b1fc2d92e04
Agrey Tosira <agreytosira@gmail.com>
2024-03-18 14:45:41 +0700

    feat: setup framer motion and add animation on dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { RiArrowDownSFill, RiArrowUpSFill } from 'react-icons/ri'
 import { FaChevronDown, FaChevronUp } from 'react-icons/fa6'
+import { AnimatePresence, motion } from 'framer-motion'
 
 const Dropdown = ({ id, children, items, className, type }) => {
   const [isOpen, setIsOpen] = useState(false)
@@ -17,17 +18,27 @@ const Dropdown = ({ id, children, items, className, type }) => {
         </button>
       </div>
 
-      {isOpen && (
-        <div className='absolute left-0 z-50 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none' role='menu' aria-orientation='vertical' aria-labelledby={`dropdown-${id}`}>
-          <div className='py-1' role='none'>
-            {items.map((item, index) => (
-              <a key={index} href='#' className='block px-4 py-2 text-sm font-medium text-gray-700 duration-200 transtion-all ease hover:bg-blue-100 hover:text-blue-600' role='menuitem'>
-                {item}
-              </a>
-            ))}
-          </div>
-        </div>
-      )}
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0, translateY: 10 }}
+            animate={{ opacity: 1, translateY: 0 }}
+            exit={{ opacity: 0, translateY: 10 }}
+            transition={{ duration: 0.2 }}
+            className='absolute left-0 z-50 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'
+            role='menu'
+            aria-orientation='vertical'
+            aria-labelledby={`dropdown-${id}`}>
+            <div className='py-1' role='none'>
+              {items.map((item, index) => (
+                <a key={index} href='#' className='block px-4 py-2 text-sm font-medium text-gray-700 duration-200 transtion-all ease hover:bg-blue-100 hover:text-blue-600' role='menuitem'>
+                  {item}
+                </a>
+              ))}
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
